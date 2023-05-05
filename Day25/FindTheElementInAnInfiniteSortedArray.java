9bd0d4598d09e0c09aaccec90969b11950ddbd74
tinamajumder <tina.majumder@nagarro.com>
2023-05-05 12:09:50 +0530

    Binary search problem solution

--- Day25/FindTheElementInAnInfiniteSortedArray.java
+++ Day25/FindTheElementInAnInfiniteSortedArray.java
@@ -1,7 +1,7 @@
 package Day25;
 import java.util.*;
 
-public class FindTheElementInAnInfinteSortedArray {
+public class FindTheElementInAnInfiniteSortedArray {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
