c90a6df87919e59aeb418a98c1b40d63497ba6c6
tinamajumder <tina.majumder@nagarro.com>
2023-06-04 19:11:06 +0530

    Day 48

---
+++ Day48/ValidParentheses.java
@@ -0,0 +1,45 @@
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+ 
+
+// Example 1:
+
+// Input: s = "()"
+// Output: true
+// Example 2:
+
+// Input: s = "()[]{}"
+// Output: true
+// Example 3:
+
+// Input: s = "(]"
+// Output: false
+
+package Day48;
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>(); // create an empty stack
+        for (char c : s.toCharArray()) { // loop through each character in the string
+            if (c == '(') // if the character is an opening parenthesis
+                stack.push(')'); // push the corresponding closing parenthesis onto the stack
+            else if (c == '{') // if the character is an opening brace
+                stack.push('}'); // push the corresponding closing brace onto the stack
+            else if (c == '[') // if the character is an opening bracket
+                stack.push(']'); // push the corresponding closing bracket onto the stack
+            else if (stack.isEmpty() || stack.pop() != c) // if the character is a closing bracket
+                // if the stack is empty (i.e., there is no matching opening bracket) or the top of the stack
+                // does not match the closing bracket, the string is not valid, so return false
+                return false;
+        }
+        // if the stack is empty, all opening brackets have been matched with their corresponding closing brackets,
+        // so the string is valid, otherwise, there are unmatched opening brackets, so return false
+        return stack.isEmpty();
+    }
+}
