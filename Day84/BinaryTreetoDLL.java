74c486a0c584e272f3a51a713d0e02adc5c61e5d
tinamajumder <tina.majumder@nagarro.com>
2023-07-28 13:04:15 +0530

    Day 84

---
+++ Day84/BinaryTreetoDLL.java
@@ -0,0 +1,214 @@
+// Given a Binary Tree (BT), convert it to a Doubly Linked List(DLL) In-Place. The left and right pointers in nodes are to be used as previous and next pointers respectively in converted DLL. The order of nodes in DLL must be same as Inorder of the given Binary Tree. The first node of Inorder traversal (leftmost node in BT) must be the head node of the DLL.
+
+// Note: H is the height of the tree and this space is used implicitly for the recursion stack.
+ 
+
+// TreeToList
+
+// Example 1:
+
+// Input:
+//       1
+//     /  \
+//    3    2
+// Output:
+// 3 1 2 
+// 2 1 3 
+// Explanation: DLL would be 3<=>1<=>2
+// Example 2:
+
+// Input:
+//        10
+//       /   \
+//      20   30
+//    /   \
+//   40   60
+// Output:
+// 40 20 60 10 30 
+// 30 10 60 20 40
+// Explanation:  DLL would be 
+// 40<=>20<=>60<=>10<=>30.
+// Your Task:
+// You don't have to take input. Complete the function bToDLL() that takes root node of the tree as a parameter and returns the head of DLL . The driver code prints the DLL both ways.
+
+// Expected Time Complexity: O(N).
+// Expected Auxiliary Space: O(H).
+
+package Day84;
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node
+{
+	Node left, right;
+	int data;
+	
+	Node(int d)
+	{
+		data = d;
+		left = right = null;
+	}
+	
+}
+class BT_To_DLL
+{
+    
+    static Node buildTree(String str)throws IOException{
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+
+	void inorder(Node node)
+	{
+		if(node==null)
+			return ;
+		else
+			inorder(node.left);
+			System.out.print(node.data + " ");
+			inorder(node.right);
+	}
+	public static void printList(Node head) 
+    {
+		Node prev = head;
+        while (head != null) 
+        {
+            System.out.print(head.data + " ");
+			prev = head;
+            head = head.right;
+        }
+		
+		System.out.println();
+		while(prev != null)
+		{
+			System.out.print(prev.data+" ");
+			prev = prev.left;
+		}
+    }
+	public static void main(String args[])  throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Solution g = new Solution();
+			    
+			    Node ans = g.bToDLL(root);
+			    printList(ans);
+                t--;
+                System.out.println();
+	        }
+			
+		
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* class Node
+class Node
+{
+	Node left, right;
+	int data;
+	
+	Node(int d)
+	{
+		data = d;
+		left = right = null;
+	}
+	
+}*/
+
+//This function should return head to the DLL
+
+class Solution {
+    Node prev = null; // Pointer to keep track of the previous node during traversal
+    Node head = null; // Pointer to the head of the doubly linked list
+
+    // Function to convert binary tree to doubly linked list and return it.
+    Node bToDLL(Node root) {
+        if (root == null)
+            return null; // Base case: if the root is null, return null (end of recursion)
+
+        bToDLL(root.left); // Recursively convert the left subtree
+
+        if (prev == null)
+            head = root; // If the previous node is null, it means this is the leftmost node (head of the list)
+        else {
+            root.left = prev; // Set the left pointer of the current root node to the previous node
+            prev.right = root; // Set the right pointer of the previous node to the current root node
+        }
+
+        prev = root; // Update the previous node to the current root node
+
+        bToDLL(root.right); // Recursively convert the right subtree
+
+        return head; // Return the head of the doubly linked list
+    }
+}
+
+
---
+++ Day84/BottomViewofBinaryTree.java
@@ -0,0 +1,220 @@
+// Given a binary tree, print the bottom view from left to right.
+// A node is included in bottom view if it can be seen when we look at the tree from bottom.
+
+//                       20
+//                     /    \
+//                   8       22
+//                 /   \        \
+//               5      3       25
+//                     /   \      
+//                   10    14
+
+// For the above tree, the bottom view is 5 10 3 14 25.
+// If there are multiple bottom-most nodes for a horizontal distance from root, then print the later one in level traversal. For example, in the below diagram, 3 and 4 are both the bottommost nodes at horizontal distance 0, we need to print 4.
+
+//                       20
+//                     /    \
+//                   8       22
+//                 /   \     /   \
+//               5      3 4     25
+//                      /    \      
+//                  10       14
+
+// For the above tree the output should be 5 10 4 14 25.
+
+// Note: The Input/Output format and Example given are used for the system's internal purpose, and should be used by a user for Expected Output only. As it is a function problem, hence a user should not read any input from the stdin/console. The task is to complete the function specified, and not to write the full code.
+ 
+
+// Example 1:
+
+// Input:
+//        1
+//      /   \
+//     3     2
+// Output: 3 1 2
+// Explanation:
+// First case represents a tree with 3 nodes
+// and 2 edges where root is 1, left child of
+// 1 is 3 and right child of 1 is 2.
+
+// Thus nodes of the binary tree will be
+// printed as such 3 1 2.
+// Example 2:
+
+// Input:
+//          10
+//        /    \
+//       20    30
+//      /  \
+//     40   60
+// Output: 40 20 60 30
+// Your Task:
+// This is a functional problem, you don't need to care about input, just complete the function bottomView() which takes the root node of the tree as input and returns an array containing the bottom view of the given tree.
+
+// Expected Time Complexity: O(N*logN).
+// Expected Auxiliary Space: O(N).
+
+package Day84;
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+//Contributed by Sudarshan Sharma
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node
+{
+    int data; //data of the node
+    int hd; //horizontal distance of the node
+    Node left, right; //left and right references
+ 
+    // Constructor of tree node
+    public Node(int key)
+    {
+        data = key;
+        hd = Integer.MAX_VALUE;
+        left = right = null;
+    }
+}
+
+
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t-- > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+    	        Solution ob = new Solution();
+			    ArrayList <Integer> res = ob.bottomView(root);
+			    for (Integer num : res) System.out.print (num + " ");
+		     	System.out.println();
+	        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    // Function to return a list containing the bottom view of the given tree.
+    public ArrayList<Integer> bottomView(Node root) {
+        Queue<Pair> q = new ArrayDeque<>(); // Queue to perform level-order traversal
+        Map<Integer, Integer> map = new TreeMap<>(); // Map to store horizontal distance and corresponding node data
+
+        q.add(new Pair(0, root)); // Add the root node to the queue with horizontal distance 0 (start the traversal from the root)
+
+        while (!q.isEmpty()) {
+            Pair curr = q.poll(); // Remove the first node from the queue (front of the queue)
+
+            map.put(curr.hd, curr.node.data); // Overwrite the value in the map for the current horizontal distance.
+            // As we perform level-order traversal, the last node encountered at each horizontal distance will be stored in the map.
+
+            // Add the left and right children of the current node to the queue with updated horizontal distance.
+            // Left child's horizontal distance is `hd - 1`, and right child's horizontal distance is `hd + 1`.
+            if (curr.node.left != null) {
+                q.add(new Pair(curr.hd - 1, curr.node.left));
+            }
+            if (curr.node.right != null) {
+                q.add(new Pair(curr.hd + 1, curr.node.right));
+            }
+        }
+
+        ArrayList<Integer> res = new ArrayList<>();
+        // Traverse the map in ascending order of horizontal distance (from left to right) and add the node data to the result list.
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            res.add(entry.getValue());
+        }
+        return res; // Return the list containing the bottom view of the binary tree
+    }
+
+    class Pair {
+        int hd;
+        Node node;
+
+        public Pair(int hd, Node node) {
+            this.hd = hd;
+            this.node = node;
+        }
+    }
+}
+
